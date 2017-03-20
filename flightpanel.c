bfbb42037d651088299cf3f9229172eb4fa5bbdf
Johannes Bauer <joe@johannes-bauer.com>
2017-03-20 21:32:36 +0100

    Now I2C works, but with custom defines (ugly)
    Need direct access to ADDR and AF flags of I2C SR1 register. Do I
    really? Surely not, right?

--- flightpanel.c
+++ flightpanel.c
@@ -15,6 +15,9 @@
 #include "usb.h"
 #include "timer.h"
 
+#define xAF			(1 << 10)
+#define xADDR		(1 << 1)
+
 int main(void) {
 	printf("Reset successful.\n");
 	init_usb_late();
@@ -30,11 +33,13 @@ int main(void) {
 //	volatile uint32_t i = 0;
 	uint8_t addr = 0;
 	while (true) {
-		addr++;
-		printf("Addressing %x\n", addr);
+		delay_millis(100);
 
+		addr += 0x10;
+		printf("Addressing 0x%x\n", addr);
 
-		 I2C_AcknowledgeConfig(I2C1, ENABLE);
+		I2C1->SR1 &= ~xAF;
+		I2C_AcknowledgeConfig(I2C1, ENABLE);
 		I2C_GenerateSTART(I2C1, ENABLE);
 
 		/* Test on SB Flag */
@@ -43,56 +48,52 @@ int main(void) {
 			timeout--;
 		}
 		if (timeout == 0) {
-			printf("Timeout SB\n");
+			printf("Timeout generating START.\n");
 			continue;
 		}
 
 		/* Send IOExpander address for read */
-		I2C_Send7bitAddress(I2C1, addr, I2C_Direction_Receiver);
+		I2C_Send7bitAddress(I2C1, addr | 1, I2C_Direction_Receiver);
 
+		/* Wait for address to be sent out */
+		while (!(I2C1->SR1 & (xADDR | xAF)));
+
+		if (I2C1->SR1 & xAF) {
+			/* ACK fail */
+			I2C_GenerateSTOP(I2C1, ENABLE);
+			continue;
+		}
+
+		/* Then wait for address ACK */
 		timeout = 50000;
 		while ((!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED)) && timeout) {
 			timeout--;
 		}
 		if (timeout == 0) {
-//			printf("Timeout EV\n");
 			I2C_GenerateSTOP(I2C1, ENABLE);
 			continue;
 		}
-		I2C_GenerateSTOP(I2C1, ENABLE);
 
-		printf("SUCCESS 0x%x\n", addr);
+
+		while ((!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_RECEIVED)) && timeout) {
+			timeout--;
+		}
+		if (timeout == 0) {
+			printf("Timeout at receiving.\n");
+			I2C_GenerateSTOP(I2C1, ENABLE);
+			continue;
+		}
+
+//		while (!I2C_GetFlagStatus(I2C1, I2C_FLAG_BTF)) { }
+
 		I2C_AcknowledgeConfig(I2C1, DISABLE);
+		I2C_GenerateSTOP(I2C1, ENABLE);
+		while( !I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_RECEIVED) );
 		uint8_t data = I2C_ReceiveData(I2C1);
 		printf("READ %x\n", data);
 
-	//	while (!I2C_GetFlagStatus(I2C1, I2C_FLAG_BTF)) { }
 
 
-#if 0
-  /* Test on SB Flag */
-  timeout = TIMEOUT_MAX;
-  while (I2C_GetFlagStatus(I2C1,I2C_FLAG_SB) == RESET)
-  {
-    if (timeout-- == 0) return(IOE_TimeoutUserCallback());
-  }
-
-  /* Transmit the slave address and enable writing operation */
-  I2C_Send7bitAddress(I2C1, DeviceAddr, I2C_Direction_Transmitter);
-
-  /* Test on ADDR Flag */
-  timeout = TIMEOUT_MAX;
-  while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED))
-  {
-    if (timeout-- == 0) return(IOE_TimeoutUserCallback());
-  }
-
-	//	I2C_Send7bitAddress(I2C1, 0xaa,
-
-		I2C_SendData(I2C1, RegisterAddr);
-
-#endif
-		delay_millis(1000);
 #if 0
 		i++;
 		if (i == 0x800) {
