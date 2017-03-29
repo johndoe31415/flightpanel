004a5bb20b1493a6c68f375fad3ebc8674b2021f
Johannes Bauer <joe@johannes-bauer.com>
2017-03-29 23:11:52 +0200

    Rasterization gets command line args
    Rasterizer now parses getopt(3) arguments and shows a more helpful help
    page if arguments are incorrect.

--- font-generator/font-rasterize.c
+++ font-generator/font-rasterize.c
@@ -4,11 +4,25 @@
 #include <stdlib.h>
 #include <math.h>
 #include <stdint.h>
+#include <getopt.h>
 #include <cairo.h>
 
 #include "font-rasterize.h"
 
 #define UNSET_PIXEL_VALUE		0
+
+static struct pgmopts_t {
+	cairo_antialias_t antialias;
+	double fontsize;
+	const char *fontname;
+	const char *outfilename;
+	bool verbose;
+} pgmopts = {
+	.antialias = CAIRO_ANTIALIAS_BEST,
+	.fontsize = 20,
+	.verbose = false,
+};
+
 
 void cairo_font_surface_init(struct cairo_font_surface_t *cfs, const char *fontname, double fontsize) {
 	memset(cfs, 0, sizeof(*cfs));
@@ -24,7 +38,7 @@ void cairo_font_surface_init(struct cairo_font_surface_t *cfs, const char *fontn
 	cairo_set_source_rgb(cfs->cctx, 0, 0, 0);
 
 	cairo_font_options_t *fopts = cairo_font_options_create();
-	cairo_font_options_set_antialias(fopts, CAIRO_ANTIALIAS_NONE);
+	cairo_font_options_set_antialias(fopts, pgmopts.antialias);
 	cairo_select_font_face(cfs->cctx, fontname ? fontname : "Sans", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL);
 	cairo_set_font_size(cfs->cctx, fontsize);
 	cairo_set_font_options(cfs->cctx, fopts);
@@ -139,6 +153,7 @@ void cairo_print_glyph(const struct glyph_t *glyph) {
 		}
 		printf("\n");
 	}
+	printf("\n");
 }
 
 void cairo_free_glyph(struct glyph_t *glyph) {
@@ -146,26 +161,85 @@ void cairo_free_glyph(struct glyph_t *glyph) {
 	memset(glyph, 0, sizeof(*glyph));
 }
 
+static void show_syntax(const char *pgmname, const char *errmsg) {
+	if (errmsg) {
+		fprintf(stderr, "error: %s\n\n", errmsg);
+	}
+	fprintf(stderr, "%s [--antialias=default|none|gray|subpixel|fast|good|best] [-s fontsize] (fontname) (outfile.json)\n", pgmname);
+	fprintf(stderr, "\n");
+	fprintf(stderr, "  -a, --antialias=VALUE   Set the Cairo antialiasing mode for fonts to either default, none, gray,\n");
+	fprintf(stderr, "                          subpixel, fast, good or best.");
+	fprintf(stderr, "  -s, --fontsize SIZE     Set the font size which is used.\n");
+	fprintf(stderr, "  -v                      Increase verbosity durign generation of rendered font.\n");
+	fprintf(stderr, "  (fontname)              Name of the font that should be rendered.\n");
+	fprintf(stderr, "  (outfile.json)          Name of the output JSON file.\n");
+}
+
+static bool parse_cmdline_options(int argc, char **argv) {
+	const struct option long_options[] = {
+		{ "antialias",	required_argument,	0, 'a' },
+		{ "fontsize",	required_argument,	0, 's' },
+		{ "verbose",	no_argument,		0, 'v' },
+		{ 0 }
+	};
+	int opt;
+	while ((opt = getopt_long(argc, argv, "a:s:v", long_options, NULL)) != -1) {
+		switch (opt) {
+			case 'a':
+				if (!strcmp(optarg, "default")) {
+					pgmopts.antialias = CAIRO_ANTIALIAS_DEFAULT;
+				} else if (!strcmp(optarg, "none")) {
+					pgmopts.antialias = CAIRO_ANTIALIAS_NONE;
+				} else if (!strcmp(optarg, "gray")) {
+					pgmopts.antialias = CAIRO_ANTIALIAS_GRAY;
+				} else if (!strcmp(optarg, "subpixel")) {
+					pgmopts.antialias = CAIRO_ANTIALIAS_SUBPIXEL;
+				} else if (!strcmp(optarg, "fast")) {
+					pgmopts.antialias = CAIRO_ANTIALIAS_FAST;
+				} else if (!strcmp(optarg, "good")) {
+					pgmopts.antialias = CAIRO_ANTIALIAS_GOOD;
+				} else if (!strcmp(optarg, "best")) {
+					pgmopts.antialias = CAIRO_ANTIALIAS_BEST;
+				} else {
+					show_syntax(argv[0], "argument given to option \"antialias\" is unknown.");
+					exit(EXIT_FAILURE);
+				}
+				break;
+
+			case 's':
+				pgmopts.fontsize = atof(optarg);
+				break;
+
+			case 'v':
+				pgmopts.verbose = true;
+				break;
+		}
+	}
+
+	if (optind + 2 != argc) {
+		show_syntax(argv[0], "expected exactly two positional arguments.");
+		exit(EXIT_FAILURE);
+	}
+	pgmopts.fontname = argv[optind];
+	pgmopts.outfilename = argv[optind + 1];
+	return true;
+}
+
 int main(int argc, char **argv) {
-	if (argc != 4) {
-		fprintf(stderr, "%s [Fontname] [Fontsize] [Outfile]\n", argv[0]);
+	if (!parse_cmdline_options(argc, argv)) {
+		show_syntax(argv[0], NULL);
 		exit(EXIT_FAILURE);
 	}
 
-	const char *fontname = argv[1];
-	double fontsize = atof(argv[2]);
-	const char *outfilename = argv[3];
-
 	struct cairo_font_surface_t cfs;
-	cairo_font_surface_init(&cfs, fontname, fontsize);
+	cairo_font_surface_init(&cfs, pgmopts.fontname, pgmopts.fontsize);
 
-	FILE *f = fopen(outfilename, "w");
+	FILE *f = fopen(pgmopts.outfilename, "w");
 
 	fprintf(f, "{\n");
-	fprintf(f, "	\"fontname\": \"%s\",\n", fontname);
-	fprintf(f, "	\"fontsize\": \"%.3f\",\n", fontsize);
+	fprintf(f, "	\"fontname\": \"%s\",\n", pgmopts.fontname);
+	fprintf(f, "	\"fontsize\": \"%.3f\",\n", pgmopts.fontsize);
 	fprintf(f, "	\"glyphs\": [\n\t\t");
-
 
 	for (int i = 32; i < 127; i++) {
 		char text[4];
@@ -174,6 +248,9 @@ int main(int argc, char **argv) {
 		struct glyph_t glyph;
 		cairo_font_surface_blit_text(&cfs, text, &glyph);
 		cairo_glyph_to_json(f, &glyph);
+		if (pgmopts.verbose) {
+			cairo_print_glyph(&glyph);
+		}
 		fprintf(f, ",\n\t\t");
 		cairo_free_glyph(&glyph);
 	}
@@ -189,6 +266,9 @@ int main(int argc, char **argv) {
 		struct glyph_t glyph;
 		cairo_font_surface_blit_text(&cfs, special_chars[i], &glyph);
 		cairo_glyph_to_json(f, &glyph);
+		if (pgmopts.verbose) {
+			cairo_print_glyph(&glyph);
+		}
 		if (i != special_char_cnt - 1) {
 			fprintf(f, ",\n\t\t");
 		}
