e1ff974e32d2b37f846286022c11bbc8972082aa
Johannes Bauer <joe@johannes-bauer.com>
2017-04-08 14:42:16 +0200

    Define VHF and NAV frequencies
    108 and 118 MHz and their respective ranges defined in instruments.h

--- firmware/instruments.h
+++ firmware/instruments.h
@@ -1,6 +1,14 @@
 #ifndef __INSTRUMENTS_H__
 #define __INSTRUMENTS_H__
 
+#define VHF_BASE_FREQUENCY			118000
+#define VHF_DETENT					25
+#define VHF_BANDWIDTH_DETENTS		(19 * 40)
+
+#define NAV_BASE_FREQUENCY			108000
+#define NAV_DETENT					50
+#define NAV_BANDWIDTH_DETENTS		(10 * 20)
+
 /*************** AUTO GENERATED SECTION FOLLOWS ***************/
 struct instrument_state;
 struct flight_data;
