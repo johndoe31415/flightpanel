3749c9161d3cdf4734be44e86d3537d205c3aeb2
Johannes Bauer <joe@johannes-bauer.com>
2017-04-11 19:03:16 +0200

    Add GPL-3 license headers
    All source code is licensed under GPLv3. The headers now reflect this
    accurately.

--- firmware/bootstrap.sh
+++ firmware/bootstrap.sh
@@ -1,6 +1,26 @@
 #!/bin/bash -e
 #
+#	flightpanel - A Cortex-M4 based USB flight panel for flight simulators.
+#	Copyright (C) 2017-2017 Johannes Bauer
 #
+#	This file is part of flightpanel.
+#
+#	flightpanel is free software; you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation; this program is ONLY licensed under
+#	version 3 of the License, later versions are explicitly excluded.
+#
+#	flightpanel is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	You should have received a copy of the GNU General Public License
+#	along with flightpanel; if not, write to the Free Software
+#	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+#	Johannes Bauer <JohannesBauer@gmx.de>
+
 
 PERIPH_ZIPFILE="en.stm32f4_dsp_stdperiph_lib.zip"
 PERIPH_SUBDIR="STM32F4xx_DSP_StdPeriph_Lib_V1.8.0"
