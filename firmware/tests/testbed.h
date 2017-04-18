4dc351b5cfa568701d554794c0ad570b02ed028b
Johannes Bauer <joe@johannes-bauer.com>
2017-04-18 22:21:25 +0200

    Change assertions to only eval once
    We want to rely on the macro being evaluated only once, so it behaves
    just like a function would. Previously this wasn't the case.

--- firmware/tests/testbed.h
+++ firmware/tests/testbed.h
@@ -8,8 +8,8 @@ typedef char* (*failfnc_t)(const void *lhs, const void *rhs);
 
 #define test_fail_if(cond)			if (cond) test_fail(__FILE__, __LINE__, #cond " was true")
 #define test_fail_unless(cond)		if (!(cond)) test_fail(__FILE__, __LINE__, #cond " was false")
-#define test_assert_str_eq(a, b)	if (strcmp((a), (b))) test_fail_ext(__FILE__, __LINE__, #a " != " #b, testbed_failfnc_str, (a), (b))
-#define test_assert_int_eq(a, b)	if ((a) != (b)) { int _a = (a); int _b = (b); test_fail_ext(__FILE__, __LINE__, #a " != " #b, testbed_failfnc_int, &_a, &_b); }
+#define test_assert_str_eq(a, b)	{ const char *_a = (a); const char *_b = (b); if (strcmp(_a, _b)) test_fail_ext(__FILE__, __LINE__, #a " != " #b, testbed_failfnc_str, _a, _b); }
+#define test_assert_int_eq(a, b)	{ int _a = (a); int _b = (b); if (_a != _b) test_fail_ext(__FILE__, __LINE__, #a " != " #b, testbed_failfnc_int, &_a, &_b); }
 
 /*************** AUTO GENERATED SECTION FOLLOWS ***************/
 bool test_verbose(void);
