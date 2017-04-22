67dacc494353c593889bf4db4c3f1e4732efed59
Johannes Bauer <joe@johannes-bauer.com>
2017-04-22 18:47:32 +0200

    Add support for IOMultiplex output chain
    Now also the 595 outputs work (pretty much out-of-the-box). Only needed
    in implement STCP pulses. Also include forgotten init.h (this broke the
    build).

---
+++ firmware/init.h
@@ -0,0 +1,11 @@
+#ifndef __INIT_H__
+#define __INIT_H__
+
+#include <stdbool.h>
+
+/*************** AUTO GENERATED SECTION FOLLOWS ***************/
+void reinit_iomux_spi_sck_AF(bool use_af);
+void SystemInit(void);
+/***************  AUTO GENERATED SECTION ENDS   ***************/
+
+#endif
