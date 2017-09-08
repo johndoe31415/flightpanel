d0599881cbe91d2bb63e4bcff62363b9556cb3ff
Johannes Bauer <joe@johannes-bauer.com>
2017-09-08 09:09:51 +0200

    More sensible debugging output in travis.py
    Omit some uneeded info (tar -v), but include some more relevant info
    (like what steps are processed). Output some header before lines to make
    them more easily visible.

--- .travis.py
+++ .travis.py
@@ -136,30 +136,34 @@ def updated_env(update_dict):
 	env.update(update_dict)
 	return env
 
+def msg(text):
+	print("==> %s" % (text))
+
 cache_dir = os.getenv("HOME") + "/.cache/flightpanel/"
 cache_compiler_file = cache_dir + "compiler.tar.gz"
 if not os.path.isfile(cache_compiler_file):
 	# Download and extract cache from upstream
 	try:
-		print("Downloading toolchain from CACHE_FILE_UPSTREAM_URI...")
+		msg("Downloading toolchain from CACHE_FILE_UPSTREAM_URI...")
 		subprocess.check_call([ "wget", "-o", "/dev/null", "-O", "cache.tar", os.getenv("CACHE_FILE_UPSTREAM_URI") ])
-		print("Download of toolchain finished successfully from CACHE_FILE_UPSTREAM_URI.")
+		msg("Download of toolchain finished successfully from CACHE_FILE_UPSTREAM_URI.")
 	except subprocess.CalledProcessError:
 		# Catch and exit so that stacktrace does not reveal URI in log
-		print("Call to wget failed while trying to download the cache file from CACHE_FILE_UPSTREAM_URI.")
-		print("Not printing out further details since the URI is hidden.")
-		print("Check that the URI is accessible (permissions?).")
+		msg("Call to wget failed while trying to download the cache file from CACHE_FILE_UPSTREAM_URI.")
+		msg("Not msging out further details since the URI is hidden.")
+		msg("Check that the URI is accessible (permissions?).")
 		sys.exit(1)
 	subprocess.check_call([ "tar", "-x", "-v", "-C", os.getenv("HOME"), "-f", "cache.tar" ])
 else:
-	print("Not downloading toolchain from CACHE_FILE_UPSTREAM_URI, using cached: %s" % (cache_compiler_file))
+	msg("Not downloading toolchain from CACHE_FILE_UPSTREAM_URI, using cached: %s" % (cache_compiler_file))
 
 compiler_dir = os.getenv("HOME") + "/bin/gcc-cm4/bin"
 if not os.path.isdir(compiler_dir):
 	# Extract compiler from cache
-	subprocess.check_call([ "tar", "-x", "-v", "-z", "-C", os.getenv("HOME"), "-f", cache_compiler_file ])
+	msg("Extracting compiler from %s" % (cache_compiler_file))
+	subprocess.check_call([ "tar", "-x", , "-z", "-C", os.getenv("HOME"), "-f", cache_compiler_file ])
 else:
-	print("Not extracting Cortex-M4 compiler, already present: %s" % (compiler_dir))
+	msg("Not extracting Cortex-M4 compiler, already present: %s" % (compiler_dir))
 
 os.environ["PATH"] = compiler_dir + ":" + os.getenv("PATH")
 
@@ -168,21 +172,30 @@ if (len(sys.argv) == 1) or ("f" in sys.argv[1]):
 	with WorkDir("firmware"):
 		shutil.copy(cache_dir + "en.stm32cubef4.zip", "en.stm32cubef4.zip")
 		shutil.copy(cache_dir + "en.stm32f4_dsp_stdperiph_lib.zip", "en.stm32f4_dsp_stdperiph_lib.zip")
+		msg("Running bootstrap.sh script")
 		subprocess.check_call([ "./bootstrap.sh" ])
 		with WorkDir("ext-st"):
+			msg("Building ST external peripheral library")
 			subprocess.check_call([ "make" ], env = updated_env({ "CFLAGS": "-DRELEASE_BUILD" }))
 		with WorkDir("cube"):
+			msg("Building ST cube library")
 			subprocess.check_call([ "make" ], env = updated_env({ "CFLAGS": "-DRELEASE_BUILD" }))
+		msg("Running firmware tests")
 		subprocess.check_call([ "make", "clean", "tests" ])
+		msg("Building firmware")
 		subprocess.check_call([ "make", "clean", "all" ], env = updated_env({ "CFLAGS": "-DRELEASE_BUILD" }))
 
 if (len(sys.argv) == 1) or ("p" in sys.argv[1]):
 	# Build (p)lugin
 	with WorkDir("fs-plugin"):
-		subprocess.check_call([ "tar", "xfvz", cache_dir + "sdk-simconnect.tar.gz" ])
-		subprocess.check_call([ "tar", "xfvz", cache_dir + "xsquawkbox.tar.gz" ])
+		msg("Extracting SimConnect header files")
+		subprocess.check_call([ "tar", "xfz", cache_dir + "sdk-simconnect.tar.gz" ])
+		msg("Extracting XSquawkbox header files")
+		subprocess.check_call([ "tar", "xfz", cache_dir + "xsquawkbox.tar.gz" ])
+		msg("Bootstrapping libhidapi")
 		subprocess.check_call([ "./bootstrap-hidapi.py" ])
 		for variant in [ "linux-emulator", "linux-xplane", "windows-fsx" ]:
+			msg("Building connector plugin for variant %s" % (variant))
 			subprocess.check_call([ "make", "clean", "all", "install" ], env = updated_env({ "VARIANT": variant }))
 
 asset_filename = "flightpanel-devbuild.tar.gz"
@@ -190,6 +203,7 @@ now_utc = datetime.datetime.utcnow()
 
 if (len(sys.argv) == 1) or ("a" in sys.argv[1]):
 	# Build (a)rtifact
+	msg("Preparing devbuild package from all binaries and some metadata")
 	dev_dir = "flightpanel-devbuild/"
 	try:
 		shutil.rmtree(dev_dir)
@@ -241,7 +255,8 @@ if (len(sys.argv) == 1) or ("a" in sys.argv[1]):
 			filename = basedir + "/" + filename
 			os.chmod(filename, 0o644)
 
-	subprocess.check_call([ "tar", "cfvz", asset_filename, dev_dir ])
+	msg("Packaging everything into %s" % (asset_filename))
+	subprocess.check_call([ "tar", "cfz", asset_filename, dev_dir ])
 
 if (len(sys.argv) == 1) or ("u" in sys.argv[1]):
 	# (u)pload build artifact to GitHub
@@ -252,30 +267,30 @@ if (len(sys.argv) == 1) or ("u" in sys.argv[1]):
 	asset_content_type = "application/x-gtar-compressed"
 	with open(asset_filename, "rb") as f:
 		asset_data = f.read()
+	msg("Read asset from %s build from commit %s (%d bytes total)" % (asset_filename, git_rev, len(asset_data)))
 
-	print("Uploading asset for revision %s (%d bytes)" % (git_rev, len(asset_data)))
-	print("Updating ref of tag %s to %s" % (tagname, git_rev))
+	msg("Updating ref of tag %s to point to %s" % (tagname, git_rev))
 	result = github.update_ref("tags/" + tagname, git_rev)
 	if result.status_code == 422:
 		# No reference yet, create
-		print("Creating reference for '%s'" % (tagname))
+		msg("Creating reference for '%s'" % (tagname))
 		result = github.create_ref("tags/" + tagname, git_rev)
 		if result.status_code != 201:
 			raise Exception("Unexpected response when trying to create_ref: %s" % (result))
 	elif result.status_code != 200:
 		raise Exception("Unexpected response when trying to update_ref: %s" % (result))
 
-	print("Getting release by tagname %s" % (tagname))
+	msg("Getting release on GibHub by the tag name of %s" % (tagname))
 	release = github.get_release_by_tagname(tagname)
 	if release.status_code == 404:
 		# No release yet, create
-		print("Creating release for '%s'" % (tagname))
+		msg("Creating release on GitHub for '%s'" % (tagname))
 		release = github.create_release(tagname, prerelease = True)
 		if release.status_code != 201:
 			raise Exception("Unexpected response when trying to create_release: %s" % (result))
 
 	release = release.json()
-	print("Editing release")
+	msg("Updating release on GitHub to contain current timestamp")
 	result = github.edit_release(release["id"], name = "Automatic release build", body = "Automatically built by Travis CI on %s UTC." % (now_utc.strftime("%A, %Y-%m-%d %H:%M:%S")))
 	if result.status_code != 200:
 		raise Exception("Unexpected response when trying to edit_release: %s" % (result))
@@ -284,12 +299,12 @@ if (len(sys.argv) == 1) or ("u" in sys.argv[1]):
 		for asset in release["assets"]:
 			if asset["name"] == asset_filename:
 				asset_id = asset["id"]
-				print("Deleting asset %s" % (asset_id))
+				msg("Deleting asset %s" % (asset_id))
 				result = github.release_delete_asset(asset_id)
 				if result.status_code != 204:
 					raise Exception("Unexpected response when trying to delete_release: %s" % (result))
 
-	print("Uploading asset")
+	msg("Uploading asset %s to GitHub" % (asset_filename))
 	result = github.release_upload_asset(release["upload_url"], asset_content_type, asset_filename, asset_data)
 	if result.status_code != 201:
 		raise Exception("Unexpected response when trying to upload_asset: %s" % (result))
