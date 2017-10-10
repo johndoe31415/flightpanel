8d4d2dac375d41f956665af186f844fb7fcf8934
Johannes Bauer <joe@johannes-bauer.com>
2017-10-10 20:21:00 +0200

    Added forgotten header file
    Poor morse.h got left behind. Fear not, morse.h. Here you go to your
    other source code files in Git.

---
+++ firmware/morse.h
@@ -0,0 +1,8 @@
+#ifndef __MORSE_H__
+#define __MORSE_H__
+
+/*************** AUTO GENERATED SECTION FOLLOWS ***************/
+const char *morse_get_code(const char letter);
+/***************  AUTO GENERATED SECTION ENDS   ***************/
+
+#endif
